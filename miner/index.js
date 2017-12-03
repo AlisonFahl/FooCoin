fe8974e4f2c026c4458dfd9add65ea8b75b765dd
GATEC\alison <alison@gatec.com.br>
2017-12-03 17:36:48 -0200

    changes to miner

--- miner/index.js
+++ miner/index.js
@@ -1,25 +1,54 @@
 var fs = require('fs');
 var rsaSign = require("jsrsasign");
+var server = require('http').createServer();
+var io = require('socket.io')(server);
 
 var Block = require("./../core/block.js");
-var Blockchain = require("./..//core/blockchain.js");
-var Transaction = require("./..//core/transaction.js");
+var Blockchain = require("./../core/blockchain.js");
+var Transaction = require("./../core/transaction.js");
 
 var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
 var blockchain;
 var transactionPool = [];
 
+io.listen(3000);
+
+io.on("connection", function(socket){
+	socket.on("add transaction", function(data){
+		var transaction = new Transaction(data.from, data.to, data.amount, data.uuid);
+		transaction.hash = data.hash;
+		
+		if(isTransactionValid(transaction) && blockchain.getBalance(transaction.from) >= transaction.amount && blockchain.containsTransaction(transaction.hash)){
+			transactionPool.push(transaction);
+			console.log("Added transaction " + transaction.hash + " to the pool.");
+		}
+		else{
+			console.log("Rejected transaction " + transaction.hash);
+		}
+	});
+	socket.on("get balance", function(address){
+		socket.emit("balance", blockchain.getBalance(address));
+	});
+});
+
 function loadBlockchain(cb){
+	console.log("Loading blockchain from disk");
+	
 	var blockchainJson = JSON.parse(fs.readFileSync(config.storage, 'utf8'));
 	
 	var blocks = blockchainJson.map(function(e){
-		return new Block(e.data, e.minerAddress, e.previousHash, e.hash, e.powNumber);
+		var transaction;
+		if(e.transaction){
+			var transaction = new Transaction(e.transaction.from, e.transaction.to, e.transaction.amount, e.transaction.uuid);
+			transaction.hash = e.transaction.hash;
+		}
+		return new Block(transaction, e.minerAddress, e.previousHash, e.hash, e.powNumber);
 	});
 	
 	blockchain = new Blockchain(blocks);
 	
-	blockchain.validate();
+	console.log("Finished loading blockchain");
 	
 	cb();
 }
@@ -28,19 +57,13 @@ function mine(){
 	while(true){
 		let transaction = "";
 		
-		while(transactionPool.length > 0){
+		if(transactionPool.length > 0){
 			transaction = transactionPool.shift();
-			if(isTransactionValid(transaction)){
-				break;
-			}
-			else{
-				transaction = "";
-			}
 		}
 		
 		var lastBlock = blockchain.chain[blockchain.chain.length - 1];
 		var newBlock = new Block(transaction, config.address, lastBlock.hash);
-		console.log("Mining a block");
+		console.log("Mining block " + blockchain.chain.length + "...");
 		newBlock.mineBlock();
 		console.log("Block mined");
 		blockchain.chain.push(newBlock);
