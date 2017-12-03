f4605c83b9ed7b4b8b709c2d2951d65d9ccdf618
GATEC\alison <alison@gatec.com.br>
2017-12-03 21:38:33 -0200

    added payment to wallet

--- wallet/index.js
+++ wallet/index.js
@@ -20,6 +20,7 @@ if(argv.g){
 //Check address balance
 if(argv.b){
 	var socket = io("http://localhost:3000");
+	
 	socket.on("connect", function(){
 		socket.emit("get balance", argv.b);
 	});
@@ -31,4 +32,35 @@ if(argv.b){
 		process.exit(0);
 	});
 	socket.emit("get balance", argv.b);
+}
+
+//Send payment
+if(argv.p){
+	var socket = io("http://localhost:3000");
+	
+	socket.on("connect", function(){
+		var transaction = new Transaction(argv.from, argv.to, argv.amount, require("uuid/v4")());
+		
+		var ec = new rsaSign.KJUR.crypto.ECDSA({'curve': 'secp256r1'});
+		
+		transaction.hash = ec.signHex(rsaSign.KJUR.crypto.Util.sha256(transaction.toString()), argv.secret)
+		console.log("Transaction hash: " + transaction.hash);
+		
+		console.log("Adding transaction to the pool.");
+		socket.emit("add transaction", transaction);
+	});
+	
+	socket.on("transaction add result", function(success){
+		if(success){
+			console.log("Transaction confirmed... Waiting to be added in a block.");
+		}
+		else{
+			console.log("Transaction rejected.");
+		}
+		process.exit(1);
+	});
+	
+	socket.on("disconnect", function(){
+		process.exit(0);
+	});
 }
